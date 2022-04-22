ba832aa617739b6b7f1988d2bd516c31d27af7cc
JeanBerly <68441010+JeanBerl@users.noreply.github.com>
2022-04-22 08:43:28 -0300

    Create relationships between Produtor.cs, Artista.cs and Album.cs

--- Artista.cs
+++ Artista.cs
@@ -2,6 +2,7 @@ using System.DateTime
 namespace Lista01;
 
 class Artista : Pessoa{
-    public DateTime DataDoContrato{get; private set;}
-    public string Genero{get; private set;}
+    public DateTime DataDoContrato {get; private set;}
+    public string Genero {get; private set;}
+    public Album[] Albuns {get; private set;}
 }
--- Produtor.cs
+++ Produtor.cs
@@ -1,5 +1,6 @@
 namespace Lista01;
-class Produtor{
-    public string Produtora{get; private set;}
-    public string[] Generos{get; private set;}
+class Produtor : Pessoa{
+    public string Produtora {get; private set;}
+    public string[] Generos {get; private set;}
+    public Album[] Albuns {get; private set;}
 }
