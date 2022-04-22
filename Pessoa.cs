df715eb4603a7d6cdc102ceef25c366ff424bb53
JeanBerly <68441010+JeanBerl@users.noreply.github.com>
2022-04-22 08:29:56 -0300

    Add Pessoa.cs properties

---
+++ Pessoa.cs
@@ -0,0 +1,8 @@
+using System.DateTime
+namespace Lista01;
+class Pessoa{
+    public string Nome {get;}
+    public string CPF {public get; private set;}
+    public DateTime DataNascimento {public get; private set;}
+    public DateTime DataFalecimento {public get; private set;}
+}
