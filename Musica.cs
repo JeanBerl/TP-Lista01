1c8bdef63791267749799bf5126f693893906ab9
JeanBerly <68441010+JeanBerl@users.noreply.github.com>
2022-04-22 08:52:03 -0300

    Create Musica.cs and its properties

---
+++ Musica.cs
@@ -0,0 +1,8 @@
+using System.DateTime;
+namespace Lista01;
+class Musica{
+    public string Nome {get; private set;}
+    public TimeSpan Duracao {get; private set;}
+    public string Genero {get; private set;}
+    public DateTime DataLancamento {get; private set;}
+}
