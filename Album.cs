3e168c47ebcc3473997c0f05cdaf9c36875fdccc
JeanBerly <68441010+JeanBerl@users.noreply.github.com>
2022-04-22 08:42:55 -0300

    Create Album.cs file and its properties

---
+++ Album.cs
@@ -0,0 +1,11 @@
+using System.DateTime
+namespace Lista01;
+
+class Album{
+    public string Nome {get; private set;}
+    public string Genero {get; private set;}
+    public DateTime DataLancamento{get; private set;}
+    // Definir o tipo de capa, pode ser string com url pra imagem?
+    public string capa {get; private set;}
+    public Musica[] musicas {get; private set;}
+}
