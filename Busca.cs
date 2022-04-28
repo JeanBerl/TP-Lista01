b6f4ddc6f089360b0296881a54f62da275e85c44
JeanBerly <68441010+JeanBerl@users.noreply.github.com>
2022-04-28 12:40:33 -0300

    Update busca.cs

--- Busca.cs
+++ Busca.cs
@@ -1,19 +1,42 @@
 namespace Lista01;
-
+using System.Linq;
 abstract class Busca{
     public static GetArtistAlbums(List<Artista> artistas, string nomeArtista){
         IQueryable<Album> albumsArtista = Artista a in artistas
             where a.Nome == nomeArtista
             select a.Albuns;
     }
-    public static GetAlbumsReleasedInSpecificYear(List<Artista> artistas, int ano){
-
+    public static IQueryable<Album> GetAlbumsReleasedInSpecificYear(List<Artista> artistas, int ano){
+        IQueryable<Album> albunsInSpecificYear = from artista in artistas
+                                                 from album in artistas.Albuns
+                                                 where album.DataLancamento.Year == ano; 
+                                                 select album;
+        return albunsInSpecificYear;
     }
-    public static List<Musica> GetArtistMusics(Artista artista){ 
-        IQueryable<Musica> musicas = Album a in artista.Albuns
-            select a.Albuns;
-        foreach(Musica in musicas){
-
-        }
+    public static IQueryable<Musica> GetArtistMusics(Artista artista){ 
+        List<Album> albuns = artista.Albuns;
+        IQueryable<Musica> artistMusicList = from album in artista.Albuns
+                                             from musica in album
+                                             select musica;
+        return artistMusics;
     }
+    public static IQueryable<Album> GetArtistAlbumsReleasedInSpecificYear(Artista artista, int ano){
+        IQueryable<Album> albunsInSpecificYear = from album in artista.Albuns
+                                                 where album.DataLancamento.Year == ano
+                                                 select album;
+        return albunsInSpecificYear;
+    }
+    public static IQueryable<Album> GetProdutorAlbunsWithSpecificGender(Produtor produtor, string genero){
+        IQueryable<Album> albunsWithSpecificGender = from album in produtor.Albuns
+                                                     where album.Genero == genero
+                                                     select album;
+        return albunsWithSpecificGender;
+    }
+    public static IQueryable<Album> GetProdutorAlbumsReleasedInSpecificYear(Produtor produtor, int ano){
+        IQueryable<Album> albunsInSpecificYear = from album in produtor.Albuns
+                                                 where album.DataLancamento.Year == ano
+                                                 select album;
+        return albunsInSpecificYear;
+    }
+    
 }
