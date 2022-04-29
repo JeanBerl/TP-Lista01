220a52e426e1df71da27467e5ea1285b5aa48511
JeanBerly <68441010+JeanBerl@users.noreply.github.com>
2022-04-29 07:56:34 -0300

    Add mthod OderAlbumByReleaseDate and make all methods return a List

--- Busca.cs
+++ Busca.cs
@@ -1,42 +1,48 @@
-namespace Lista01;
 using System.Linq;
+namespace Lista01;
+
 abstract class Busca{
-    public static GetArtistAlbums(List<Artista> artistas, string nomeArtista){
+    public static List<Album> GetArtistAlbums(List<Artista> artistas, string nomeArtista){
         IQueryable<Album> albumsArtista = Artista a in artistas
             where a.Nome == nomeArtista
             select a.Albuns;
+        return albumsArtista.ToList();
     }
-    public static IQueryable<Album> GetAlbumsReleasedInSpecificYear(List<Artista> artistas, int ano){
+    public static List<Album> GetAlbumsReleasedInSpecificYear(List<Artista> artistas, int ano){
         IQueryable<Album> albunsInSpecificYear = from artista in artistas
                                                  from album in artistas.Albuns
                                                  where album.DataLancamento.Year == ano; 
                                                  select album;
-        return albunsInSpecificYear;
+        return albunsInSpecificYear.ToList();
     }
-    public static IQueryable<Musica> GetArtistMusics(Artista artista){ 
+    public static List<Musica> GetArtistMusics(Artista artista){ 
         List<Album> albuns = artista.Albuns;
         IQueryable<Musica> artistMusicList = from album in artista.Albuns
                                              from musica in album
                                              select musica;
-        return artistMusics;
+        return artistMusics.ToList();
     }
-    public static IQueryable<Album> GetArtistAlbumsReleasedInSpecificYear(Artista artista, int ano){
+    public static List<Album> GetArtistAlbumsReleasedInSpecificYear(Artista artista, int ano){
         IQueryable<Album> albunsInSpecificYear = from album in artista.Albuns
                                                  where album.DataLancamento.Year == ano
                                                  select album;
-        return albunsInSpecificYear;
+        return albunsInSpecificYear.ToList();
     }
-    public static IQueryable<Album> GetProdutorAlbunsWithSpecificGender(Produtor produtor, string genero){
+    public static List<Album> GetProdutorAlbunsWithSpecificGender(Produtor produtor, string genero){
         IQueryable<Album> albunsWithSpecificGender = from album in produtor.Albuns
                                                      where album.Genero == genero
                                                      select album;
-        return albunsWithSpecificGender;
+        return albunsWithSpecificGender.ToList();
     }
     public static IQueryable<Album> GetProdutorAlbumsReleasedInSpecificYear(Produtor produtor, int ano){
         IQueryable<Album> albunsInSpecificYear = from album in produtor.Albuns
                                                  where album.DataLancamento.Year == ano
                                                  select album;
-        return albunsInSpecificYear;
+        return albunsInSpecificYear.ToList();
+    }
+    public static List<Album> OrderAlbumByReleaseDate(List<Album> albums){
+        IQueryable<Album> AlbunsOrderedByRelease = albuns.OrderBy(album => album.DataLancamento);
+        return AlbunsOrderedByRelease.ToList();
     }
     
 }
